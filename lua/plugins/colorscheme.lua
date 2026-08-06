8281c7ab2a19080030607b1c52a5bc2541799e38
Louis Chouane <chouane.louis@gmail.com>
2026-08-07 00:19:11 +0200

    update gitignore and change colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,7 @@
 return {
-	"rose-pine/neovim",
-	name = "rose-pine",
-	config = function()
-		vim.cmd("colorscheme rose-pine-moon")
-	end
+    "savq/melange-nvim",
+    priority = 1000,
+    config = function ()
+        vim.cmd("colorscheme melange")
+    end,
 }
-
